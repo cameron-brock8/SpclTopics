208ffad52a18aff953d84c77afb3c5393c3b50a0
Cameron Brock <ckb19b@fsu.edu>
2023-01-28 13:30:16 -0500

    Changing int to uint_8

--- Exercise1/filter-ascii.c
+++ Exercise1/filter-ascii.c
@@ -6,21 +6,28 @@
 int main(){
 
     int reading, count = 0;
+    bool ascii = true;
 
     do{
         count++;
-        char c;
+        uint8_t c;
         reading = read(0, &c, 1);
 
-        unsigned int ascii = c;
+        //unsigned int ascii = c;
+        //uint8_t ascii = c;
 
-        if(ascii > 127)
+        if(c > 127)
         {
-            printf("Not an ascii file, found a %u integer at byte %d", ascii, count);
+            ascii = false;
+            printf("Not an ascii file, found a %u integer at byte %d", c, count);
             printf(": %c \n", c);
         }
 
     } while(reading != 0);
 
+    if (ascii == true)
+    {
+        printf("ASCII data\n");   
+    }
 
 }
