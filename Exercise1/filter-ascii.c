2d100365a502ba5cf71fe279d8368a494dc6279e
Cameron Brock <ckb19b@fsu.edu>
2023-02-06 12:51:52 -0500

    Final changes

--- Exercise1/filter-ascii.c
+++ Exercise1/filter-ascii.c
@@ -13,16 +13,12 @@ int main(){
         uint8_t c;
         reading = read(0, &c, 1);
 
-        //unsigned int ascii = c;
-        //uint8_t ascii = c;
-
         if(c > 127)
         {
             ascii = false;
             printf("Not an ascii file, found a %u integer at byte %d", c, count);
             printf(": %c \n", c);
         }
-
     } while(reading != 0);
 
     if (ascii == true)
